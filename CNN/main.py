ab105309cd65978187656ccd334ff6c390e2f3ba
Dtchaves <diogo.tuler@gmail.com>
2023-12-13 13:52:54 -0300

    readme

--- CNN/main.py
+++ CNN/main.py
@@ -30,13 +30,13 @@ train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=Tru
 val_loader = DataLoader(dataset=val_data, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_data, batch_size=6, shuffle=False)
 
-test_loader
-
+model_name = "Resnet34"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"\nRodando modelo de Resnet18 na {device}")
+print(f"\nRodando modelo de {model_name} na {device}")
 
-cnn_model = Resnet18().to(device)
+cnn_model = Resnet34().to(device)
+
 entrada = ''
 while entrada != "Load" and entrada != "Train":
     entrada = input("\nLoad or Train?\n")
@@ -44,19 +44,19 @@ while entrada != "Load" and entrada != "Train":
     if entrada == "Load":
         print(f"{Fore.BLUE}\n ----- STARTING LOADING -----\n{Style.RESET_ALL}")
         
-        path_dic = '/scratch/diogochaves/Projetos/ICV/CNN/results/best_w/Best_w_Resnet18'
+        path_dic = f'/scratch/diogochaves/Projetos/ICV/CNN/results/best_w/Best_w_{model_name}'
         dic = torch.load(path_dic)
         cnn_model.load_state_dict(dic)
         
         print(f"{Fore.GREEN}LOADING COMPLETED{Style.RESET_ALL}")
         
     elif entrada == "Train":
-        trainer = Trainer(model=cnn_model,train_loader=train_loader,val_loader=val_loader,model_name="Resnet18",path_par='/scratch/diogochaves/Projetos/ICV/CNN/results/best_w',path_loss='/scratch/diogochaves/Projetos/ICV/CNN/results/loss')
+        trainer = Trainer(model=cnn_model,train_loader=train_loader,val_loader=val_loader,model_name= model_name,path_par='/scratch/diogochaves/Projetos/ICV/CNN/results/best_w',path_loss='/scratch/diogochaves/Projetos/ICV/CNN/results/loss')
         trainer.run(device=device,epochs=40)
 
 print(f"{Fore.BLUE}\n ----- STARTING TESTING -----\n{Style.RESET_ALL}")
 
-test = Test(cnn_model,test_loader,"Resnet18",path_metric='/scratch/diogochaves/Projetos/ICV/CNN/results/metrics',path_n='/scratch/diogochaves/Projetos/ICV/CNN/results/por_quantidade')
+test = Test(cnn_model,test_loader,model_name,path_metric='/scratch/diogochaves/Projetos/ICV/CNN/results/metrics',path_n='/scratch/diogochaves/Projetos/ICV/CNN/results/por_quantidade')
 classification_report = test.fit(device=device)
 print(classification_report)
 
